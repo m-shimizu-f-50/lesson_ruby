d87a4975ab8bc64164bdf33387d0df671be6e907
mst827 <k.s.m501196gmail.com>
2020-04-20 12:35:30 +0900

    [Finish]2-8(2)

---
+++ lesson7-2.rb
@@ -0,0 +1,34 @@
+puts "計算を始めます"
+puts "何回計算を繰り返しますか？"
+
+input = gets.to_i
+
+i= 0
+
+while i <= input do
+
+	puts "#{i}回目の計算"
+
+	puts "2つ目の値を入力してください"
+
+	a = gets.to_i
+	b = gets.to_i
+
+	puts "a=#{a}"
+	puts "b=#{b}"
+
+	puts "計算結果を出力します"
+
+	puts "a+b=#{a+b}"
+	puts "a-b=#{a-b}"
+	puts "a*b=#{a*b}"
+	puts "a/b=#{a/b}"
+
+	i += 1
+
+end
+
+puts "計算を終了します"
+
+
+
